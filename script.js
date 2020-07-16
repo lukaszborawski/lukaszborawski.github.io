456cea3dda540366191d3c2456e98c99a28eae25
Łukasz Borawski <lukasz.borawski@gmail.com>
2020-07-16 18:04:14 +0200

    fix burger button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const burger = document.querySelector(".menu_button");
-
+const menu = document.querySelector(".menu_list")
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-times");
 
 burger.addEventListener("click", function () {
-    iconBurger.classList.toggle("show");
-    iconX.classList.toggle("show");
+    iconBurger.classList.toggle("fas--show");
+    iconX.classList.toggle("fas--show");
+    menu.classList.toggle("menu_list--show");
 })
