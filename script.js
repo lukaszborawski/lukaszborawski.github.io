2726df9146536fd50112204c36ee48b310487dd6
Łukasz Borawski <lukasz.borawski@gmail.com>
2020-08-10 15:22:56 +0200

    fix navigation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector(".menu_button");
 const menu = document.querySelector(".menu_list")
 const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
 
 burger.addEventListener("click", function () {
     iconBurger.classList.toggle("fas--show");
-    iconX.classList.toggle("fas--show");
+    menu.classList.toggle("menu_list--show");
+})
+
+menu.addEventListener("click", function () {
     menu.classList.toggle("menu_list--show");
 })
